03a6811c3d61944e687cc45d00ff9607ec6e89e7
Kay-Rodriguez <Karla.rodriguez@epn.edu.ec>
2025-11-18 22:44:21 -0500

    comit2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'movilesKarla',
+  appId: 'com.movileskarla.app',
   appName: 'movistarexamen',
   webDir: 'www',
   bundledWebRuntime: false
